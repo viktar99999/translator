19b6d3caba0518516e0f78261c90c5245b5f6de4
viktar99999 <viktar77722@gmail.com>
2022-08-26 21:17:15 +0300

    main.py

---
+++ translator_en/translator.py
@@ -0,0 +1,12 @@
+import googletrans
+from googletrans import Translator
+file = open('/home/vik/contents.txt', 'r')
+content = file.read()
+print(content)
+translator = Translator()
+result = translator.translate(content, dest='ru')
+print(result.text)
+with open('/home/vik/text.txt', 'w') as f:
+    f.write(result.text)
+
+
