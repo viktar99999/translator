27689bf3735f93817852dae13d4ea78fa89e127b
viktar99999 <viktar77722@gmail.com>
2023-02-14 13:03:29 +0300

    upgrade 2, 8 strings

--- translator_ru/translator.py
+++ translator_ru/translator.py
@@ -1,12 +1,12 @@
 import googletrans
 from googletrans import Translator
-file = open('/home/user_name/contents.txt', 'r')
+file = open('/home/user_name/contents.txt', 'r', encoding='utf-8')
 content = file.read()
 print(content)
 translator = Translator()
 result = translator.translate(content, dest='en')
 print(result.text)
-with open('/home/user_name/text.txt', 'w') as f:
+with open('/home/user_name/text.txt', 'w', encoding='utf-8') as f:
     f.write(result.text)
 
 
