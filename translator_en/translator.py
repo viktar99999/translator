7d05652314a439847fe0d37ea45f3c67738fe0fb
viktar99999 <viktar77722@gmail.com>
2023-02-20 13:54:16 +0300

    upgrade add 5, 6 strings

--- translator_en/translator.py
+++ translator_en/translator.py
@@ -3,6 +3,8 @@ from googletrans import Translator
 file = open('/home/user_name/contents.txt', 'r', encoding='utf-8')
 content = file.read()
 print(content)
+line_count = sum(1 for line in open('/home/user_name/contents.txt', 'r', encoding='utf-8'))
+print(line_count)
 translator = Translator()
 result = translator.translate(content, dest='ru')
 print(result.text)
